ca017796373157fa2c7b042ee01aa8c6d81b4516
Chien-Chih-Wang <tony20020912@gmail.com>
2022-09-29 00:21:44 +0800

    Lab 1 Adv: update cb_2

--- lab1_advanced/lab1_advanced.srcs/sources_1/new/Lab1_Team30_Crossbar_2x2_4bit.v
+++ lab1_advanced/lab1_advanced.srcs/sources_1/new/Lab1_Team30_Crossbar_2x2_4bit.v
@@ -1,4 +1,44 @@
 `timescale 1ns/1ps
+module dmux(
+    input [3:0] in,
+    input sel,
+    output [3:0] a,
+    output [3:0] b
+    );
+
+wire not_sel;
+not(not_sel, sel);
+
+and a1[3:0](a, in, {not_sel, not_sel, not_sel, not_sel});
+and a2[3:0](b, in, {sel, sel, sel, sel});
+
+endmodule
+
+module mux(
+    input [3:0] a,
+    input [3:0] b,
+    input sel,
+    output [3:0] f
+);
+
+wire not_sel;
+not(not_sel, sel);
+
+wire [3:0] w0, w1, w2;
+
+and(w0[0], a[0], not_sel);
+and(w0[1], a[1], not_sel);
+and(w0[2], a[2], not_sel);
+and(w0[3], a[3], not_sel);
+
+and(w1[0], b[0], sel);
+and(w1[1], b[1], sel);
+and(w1[2], b[2], sel);
+and(w1[3], b[3], sel);
+
+or o[3:0](f, w0, w1);
+
+endmodule
 
 module Crossbar_2x2_4bit(in1, in2, control, out1, out2);
 input [4-1:0] in1, in2;
