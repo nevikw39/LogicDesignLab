8d60ed781c76c7b18f9dcff108f695c6b61f2ca3
Chien-Chih-Wang <tony20020912@gmail.com>
2022-09-29 00:32:12 +0800

    Lab 1 Adv: update tff

--- lab1_advanced/lab1_advanced.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
+++ lab1_advanced/lab1_advanced.srcs/sources_1/new/Lab1_Team30_Toggle_Flip_Flop.v
@@ -1,5 +1,35 @@
 `timescale 1ns/1ps
 
+module D_Latch(
+    input e,
+    input d,
+    output q
+    );
+
+wire not_d, w0, w1, not_q;
+not(not_d, d);
+nand(w0, d, e);
+nand(w1, not_d, e);
+
+nand(q, w0, not_q);
+nand(not_q, w1, q);
+
+endmodule
+
+module D_Flip_Flop(
+    input clk,
+    input d,
+    output q
+    );
+
+wire not_clk, w;
+not(not_clk, clk);
+
+D_Latch master(not_clk, d, w), slave(clk, w, q);
+
+endmodule
+
+
 module Toggle_Flip_Flop(clk, q, t, rst_n);
 input clk;
 input t;
