6e8d1f7dae3e9f29190f5a1402d602672ccf3899
Chien-Chih-Wang <tony20020912@gmail.com>
2022-09-28 11:36:46 +0800

    Lab 1 Adv: Add TFF tb

---
+++ lab1_advanced/lab1_advanced.srcs/sources_1/imports/Desktop/Lab1_Team30_Toggle_Flip_Flop_t.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module Toggle_Flip_Flop_t;
+
+// input and output signals
+reg clk = 1'b0;
+reg t = 1'b0;
+reg rst_n = 1'b0;
+wire q;
+
+// generate clk
+always#(1) clk = ~clk;
+
+// test instance instantiation
+Toggle_Flip_Flop TFF(
+    .clk(clk),
+    .t(t),
+    .rst_n(rst_n),
+    .q(q)
+);
+
+// uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
+// initial begin
+//      $fsdbDumpfile("DFF.fsdb");
+//      $fsdbDumpvars;
+// end
+
+// brute force 
+initial begin
+    @(negedge clk) rst_n = 1'b1;
+    @(negedge clk) t = 1'b1;
+    @(negedge clk) t = 1'b0;
+    @(negedge clk) $finish;
+end
+
+endmodule
