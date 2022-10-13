f086fdb527e9f80f2a5ae1c08889d61eb14ac158
nevikw39 <nevikw39@icloud.com>
2022-10-13 16:52:41 +0800

    Lab 3 Basic: Memory

--- lab3_basic/lab3_basic.srcs/sources_1/imports/LogicDesignLab/Lab3_Memory.v
+++ lab3_basic/lab3_basic.srcs/sources_1/imports/LogicDesignLab/Lab3_Memory.v
@@ -5,6 +5,21 @@ input clk;
 input ren, wen;
 input [7-1:0] addr;
 input [8-1:0] din;
-output [8-1:0] dout;
+output reg [8-1:0] dout;
+
+parameter M = 128, N = 8;
+reg [N-1:0] mem [M-1:0];
+
+always @(posedge clk) begin
+    if (ren) begin
+        dout <= mem[addr];
+    end
+    else if (wen) begin
+        dout <= 0;
+        mem[addr] <= din;
+    end
+    else
+        dout <= 0;
+end
 
 endmodule
