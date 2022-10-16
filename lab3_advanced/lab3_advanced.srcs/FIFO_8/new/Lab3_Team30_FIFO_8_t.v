779fe0047c85f671cb67fc9c3e8017b6b30a5242
nevikw39 <nevikw39@icloud.com>
2022-10-16 22:07:01 +0800

    Lab 3 Adv.: 8-bit FIFO

---
+++ lab3_advanced/lab3_advanced.srcs/FIFO_8/new/Lab3_Team30_FIFO_8_t.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Team 30
+// Engineer: nevikw39
+// 
+// Create Date: 10/16/2022 09:00:16 PM
+// Design Name: 8-bit FIFO testbench
+// Module Name: FIFO_8_t
+// Project Name: Lab 3 Advanced
+// Target Devices: Basys 3
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FIFO_8_t();
+
+reg clk = 0, rst_n = 0, ren = 1, wen = 0;
+reg[7:0] din = 0;
+wire[7:0] dout;
+wire error;
+FIFO_8 f(clk, rst_n, wen, ren, din, dout, error);
+
+// specify duration of a clock cycle.
+parameter cyc = 10;
+
+// generate clock.
+always #(cyc / 2) clk = !clk;
+
+// uncommment and add "+access+r" to your nverilog command to dump fsdb waveform on NTHUCAD
+// initial begin
+//     $dumpfile("Clock_Divider.vhd");
+//     $dumpvars("+all");
+// end
+
+initial begin
+    // 0
+    @(negedge clk)
+    rst_n = 1;
+    
+    // 1
+    @(negedge clk)
+    din = 56;
+    ren = 0;
+    wen = 1;
+    
+    // 2
+    @(negedge clk)
+    din = 11;
+    
+    // 3
+    @(negedge clk)
+    din = 42;
+    
+    // 4
+    @(negedge clk)
+    din = 10;
+    
+    // 5
+    @(negedge clk)
+    din = 23;
+    
+    // 6
+    @(negedge clk)
+    din = 20;
+    
+    // 7
+    @(negedge clk)
+    din = 6;
+    
+    // 8
+    @(negedge clk)
+    din = 85;
+    
+    // 9
+    @(negedge clk)
+    din = 45;
+    ren = 1;
+    
+    // 10
+    @(negedge clk)
+    din = 12;
+    ren = 0;
+    
+    // 11
+    @(negedge clk)
+    din = 77;
+    
+    // 12
+	@(negedge clk)
+	wen = 0;
+	
+	// 
+	@(negedge clk)
+    $finish;
+end
+
+endmodule
--- lab3_advanced/lab3_advanced.srcs/sources_1/imports/LogicDesignLab/Lab3_Team30_FIFO_8.v
+++ lab3_advanced/lab3_advanced.srcs/sources_1/imports/LogicDesignLab/Lab3_Team30_FIFO_8.v
@@ -5,7 +5,49 @@ input clk;
 input rst_n;
 input wen, ren;
 input [8-1:0] din;
-output [8-1:0] dout;
-output error;
+output reg [8-1:0] dout;
+output reg error;
+
+reg[7:0] queue[2:0];
+reg[2:0] front = 0, back = 0;
+
+assign full = (back + 1 & 7) == front;
+assign empty = !full && front == back;
+
+always @(posedge clk) begin
+    if (rst_n) begin
+        if (ren) begin
+            if (empty) begin
+                error <= 1;
+                dout <= 8'bxxxxxxxx;
+            end
+            else begin
+                error <= 0;
+                dout <= queue[front + 1 & 7];
+                front <= front + 1 & 7;
+            end
+        end
+        else if (wen) begin
+            if (full) begin
+                error <= 1;
+                dout <= 8'bxxxxxxxx;
+            end
+            else begin
+                error <= 0;
+                dout <= 8'bxxxxxxxx;
+                queue[back + 1 & 7] <= din;
+                back <= back + 1 & 7;
+            end
+        end
+        else
+            dout <= 8'bxxxxxxxx;
+     end
+     else begin
+        front <= 0;
+        back <= 0;
+        dout <= 0;
+        error <= 0;
+     end
+end
 
 endmodule
